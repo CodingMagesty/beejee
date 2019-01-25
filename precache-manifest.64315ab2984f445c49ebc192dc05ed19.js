5920a1269885877526c7d6f87315c6ae9b378349
Gleb <germolaev@team.amocrm.com>
2019-01-25 12:49:46 +0300

    asd

---
+++ precache-manifest.64315ab2984f445c49ebc192dc05ed19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3ea7397cb8fdc1694c7f",
+    "url": "/static/js/main.3ea7397c.chunk.js"
+  },
+  {
+    "revision": "1b19d8664e6083d9870e",
+    "url": "/static/js/1.1b19d866.chunk.js"
+  },
+  {
+    "revision": "3ea7397cb8fdc1694c7f",
+    "url": "/static/css/main.97919778.chunk.css"
+  },
+  {
+    "revision": "215d59d0ab9b4e27c02bdb7b6341b423",
+    "url": "/index.html"
+  }
+];
